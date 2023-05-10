4f5b5baa9f92bcdda62dfda75d4f1d9d1f991ff0
r4vk <133111741+r4vk@users.noreply.github.com>
2023-05-10 13:05:16 +0200

    Create PictureCaptureDateMismatch.py

---
+++ PictureCaptureDateMismatch.py
@@ -0,0 +1,71 @@
+import os
+import subprocess
+import csv
+
+def get_exif_date(filename):
+    """
+    Function to execute the ExifTool command and retrieve the capture date from the EXIF metadata.
+
+    Parameters:
+        filename (str): Path to the file.
+
+    Returns:
+        str or None: Capture date in the format '%Y%m%d' or None if an error occurred.
+    """
+
+    result = subprocess.run(['exiftool', '-CreateDate', '-d', '%Y%m%d', '-s3', filename], capture_output=True, text=True)
+    if result.returncode == 0:
+        return result.stdout.strip()
+    else:
+        return None
+
+def get_file_date(filename, mask, date_format):
+    """
+    Function to analyze the file name and retrieve the date according to the specified mask and format.
+
+    Parameters:
+        filename (str): File name.
+        mask (tuple): Date mask in the format (start_index, length).
+        date_format (str): Expected date format.
+
+    Returns:
+        str or None: Date from the file name in the format '%Y%m%d' or None if an error occurred.
+    """
+
+    try:
+        basename = os.path.basename(filename)
+        date_str = basename[mask[0]:mask[0]+mask[1]]  # Retrieve the portion of the file name according to the mask
+        return date_str.replace('_', '-').replace('-', '')  # Remove '_' and '-' characters
+    except ValueError:
+        return None
+
+def main():
+    """
+    Main function of the program.
+    """
+
+    # Define the file name mask and date format
+    mask = (0, 8)  # Sample mask - starts at index 0 and has a length of 8 characters
+    date_format = "%Y%m%d"  # Sample date format
+
+    # Create the CSV file
+    csv_file = open('output.csv', 'w', newline='')
+    csv_writer = csv.writer(csv_file)
+    csv_writer.writerow(['File Name', 'Location', 'File Date', 'EXIF Capture Date'])
+
+    # Traverse all files in the folder and subfolders
+    for root, dirs, files in os.walk('.'):
+        for file in files:
+            if file.lower().endswith('.jpg'):
+                filename = os.path.join(root, file)
+                file_date = get_file_date(filename, mask, date_format)
+                exif_date = get_exif_date(filename)
+                if file_date and exif_date and file_date != exif_date:
+                    csv_writer.writerow([file, filename, file_date, exif_date])
+
+    # Close the CSV file
+    csv_file.close()
+    print("Program finished. Results have been saved to output.csv.")
+
+if __name__ == "__main__":
+    main()
